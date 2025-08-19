aeab5cf8ac0411547609cb029fc9bb3a4f577dfe
PattrawutKhammaAot <140035503+PattrawutKhammaAot@users.noreply.github.com>
2025-08-19 17:03:31 +0700

    countasset

--- lib/page/main_menu.dart
+++ lib/page/main_menu.dart
@@ -162,7 +162,7 @@ class _MenuPageState extends State<MenuPage> with TickerProviderStateMixin {
                 ),
                 padding: const EdgeInsets.all(8),
                 child: Text(
-                  'Assets Count',
+                  'Count Assets',
                   style: TextStyle(
                     fontSize: 20,
                     fontWeight: FontWeight.bold,
